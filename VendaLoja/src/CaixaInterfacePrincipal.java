f472f6c6da6c9b5c77a83de68f6ddc518f0346ff
nayarocha <nayaarocha@gmail.com>
2014-05-25 18:40:06 -0300

    Atualizando atividade

--- VendaLoja/src/CaixaInterfacePrincipal.java
+++ VendaLoja/src/CaixaInterfacePrincipal.java
@@ -1,70 +1,13 @@
 import java.util.Scanner;
 
 public class CaixaInterfacePrincipal {
-	public static void main(String[] args) {
 		
-		Venda novaVenda = new Venda();
-		menu();
-		Scanner ler = new Scanner(System.in);
-		int opcao =1;
-		opcao = ler.nextInt();
-		
-			
-			switch(opcao){
-			case 1:
-				menu();
-				break;			
-			case 2:
-				
-				String nome, descricao;
-				float valor;
-				
-				Item novoItem = new Item();
-				Scanner lerNome = new Scanner(System.in);
-				System.out.println("Insira o nome do produto:");
-				nome = lerNome.nextLine();
-				
-				Scanner lerValor = new Scanner(System.in);
-				System.out.println("Insira o valor:");
-				valor = lerValor.nextFloat();
-				
-				Scanner lerDescricao = new Scanner(System.in);
-				System.out.println("Insira uma descrição:");
-				descricao = lerDescricao.nextLine();
-				
-				novoItem.setNome(nome);
-				novoItem.setValor(valor);
-				novoItem.setDescricao(descricao);
-			
-				novaVenda.registrarItem(novoItem);
-				System.out.println("item adicionado");
-				
-				menu();
-				
-				break;
-			case 3:
-			//remomover item
-				Scanner lerNomeItem = new Scanner(System.in);
-				String nomeItem;
-				nomeItem = lerNomeItem.nextLine();
-				novaVenda.removerItem(nomeItem);
-			
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 6:
-				break;
-			case 0:
-				break;					
-			default:
-				System.out.println("NÃO É UMA OPÇÃO VÁLIDA");
-		}
-	  }
-				
-	public static void menu(){
-		
+	private static Scanner ler;
+
+	public static void main(String[] args) {			
+		int num = -1;
+		Venda novaVenda = null;
+		do{
 		System.out.println("1 - INICIAR VENDA");
 		System.out.println("2 - REGISTRAR ITEM");
 		System.out.println("3 - REMOVER ITEM");
@@ -72,7 +15,79 @@ public class CaixaInterfacePrincipal {
 		System.out.println("5 - FINALIZAR VENDA (INCLUIR VER O TOTAL)");
 		System.out.println("0 - SAIR");
 		System.out.println("-------------------------------------------------------------------------");
-
+		ler = new Scanner(System.in);
+		int opcao = ler.nextInt();
+		
+		
+			
+				switch(opcao){
+					case 1:
+						novaVenda = new Venda();
+						System.out.println("Iniciando Venda...");
+						System.out.println("********************************");
+						
+						break;			
+					case 2:
+					
+						//registrar item
+						String nome, descricao;
+						float valor;
+						
+						Item novoItem = new Item();
+						Scanner lerNome = new Scanner(System.in);
+						System.out.println("Insira o nome do produto:");
+						nome = lerNome.nextLine();
+						
+						Scanner lerValor = new Scanner(System.in);
+						System.out.println("Insira o valor:");
+						valor = lerValor.nextFloat();
+						
+						Scanner lerDescricao = new Scanner(System.in);
+						System.out.println("Insira uma descrição:");
+						descricao = lerDescricao.nextLine();
+						
+						novoItem.setNome(nome);
+						novoItem.setValor(valor);
+						novoItem.setDescricao(descricao);
+					
+						novaVenda.registrarItem(novoItem);
+											
+						break;
+					case 3:
+					//remomover item
+						System.out.println("Digite o Item para Remover : \n");
+						System.out.println("********************************");
+						Scanner lerNomeItem = new Scanner(System.in);
+						String nomeItem;
+						nomeItem = lerNomeItem.nextLine();
+						novaVenda.removerItem(nomeItem);
+					
+						break;
+					case 4:
+					//ver itens registrados 
+						Item[] listar = novaVenda.verItensRegistrados();
+						System.out.println("Total de itens Registrados:" +novaVenda.controledeVendas());
+						System.out.println("----------------------------------------------------------");
+						for (int i = 0; i < listar.length; i++) {
+							if(listar[i] != null){
+								System.out.println(listar[i].getNome());
+							}
+						}
+						break;
+					case 5:
+						System.out.println("Total:" +novaVenda.calcularTotal() +" reais");
+						novaVenda = null;
+						System.out.println("********************************");
+						System.out.println("Venda Finalizada!");
+						System.out.println("********************************");
+						break;
+					case 0:
+						System.out.println("Saindo do programa");
+						num = 0;
+						break;					
+					default:
+						System.out.println("opção invalida");
+				}
+			} while(num != 0 );
 	}
-
 }
--- VendaLoja/src/Venda.java
+++ VendaLoja/src/Venda.java
@@ -1,33 +1,69 @@
 
 public class Venda {
 	//criando galpão
-	Item itensdeVenda[] = new Item[10];
+	Item[] itensdeVenda = new Item[5];	
+	int controledeVendas = 0;
 	
-	int controledeEstoque = 0;
 	
-	//private float total;
+	private float total = 0;
 
+	
 	//adicionando itens
 	public void registrarItem(Item item){
-		for (int i = 0; i < itensdeVenda.length; i++) {
-			itensdeVenda[i] = item;
-			controledeEstoque++;		
+		//aumentando tamanho do array itensdevenda
+		if(controledeVendas == itensdeVenda.length){
+			Item[] item_aux =  new Item[controledeVendas*2];
+			for (int i = 0; i < item_aux.length; i++) {
+				item_aux[i] = itensdeVenda[i];
+			}
+			itensdeVenda = item_aux;		
 		}
+		
+		//inserindo os itens no array
+		for (int i = 0; i < itensdeVenda.length; i++) {
+			if(itensdeVenda[i] == null){
+				itensdeVenda[i] = item;
+				controledeVendas++;
+				break;
+			}
+		}
+		System.out.println("item adicionado");
 	}
 	
 	//removendo itens
 	public  boolean removerItem(String nome){
 		for (int i=0; i< itensdeVenda.length; i++){
-			if(itensdeVenda[i].getNome() == nome){
+			if(itensdeVenda[i] != null && nome.equals(itensdeVenda[i].getNome())){
 				itensdeVenda[i] = null;
-				controledeEstoque--;
+				controledeVendas--;
 				System.out.println("produto removido");
 				return true;				
 			}
 		}
-		System.out.println("produto removido");
+		System.out.println("produto n removido");
 		return false;		
 	}
 	
 	
+	//listando itens registrados
+	public Item[] verItensRegistrados(){	
+		return this.itensdeVenda;			
+	}
+	
+	public float calcularTotal(){
+		
+		
+		for (int i = 0; i < itensdeVenda.length; i++) {
+			if(itensdeVenda[i] != null){
+				total += itensdeVenda[i].getValor();
+			}
+			
+		}
+		return total;
+		
+	}
+	
+	public int controledeVendas(){
+		return controledeVendas;
+	}
 }
